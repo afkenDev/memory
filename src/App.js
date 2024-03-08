693671841feb9bff0a13e392e37abdbbc5f9232b
afkenDev <kenzo.abdelkebir@gmail.com>
2024-03-08 14:06:29 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,74 +1,98 @@
-import React, { useState, useEffect} from 'react';
-import './App.css';
-import SingleCard from './components/SingleCard';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
 
 const animal_imgs = [
-  {"src": "/img/camel.png", "backgroundColor": "#ff0000", matched: false},
-  {"src": "/img/dog.png", "backgroundColor": "#0000ff", matched: false},
-  {"src": "/img/fox.png", "backgroundColor": "#ffff00", matched: false},
-  {"src": "/img/mouse.png", "backgroundColor": "#ff00ff", matched: false},
-  {"src": "/img/penguin.png", "backgroundColor": "#00ffff", matched: false},
-  {"src": "/img/rabbit.png", "backgroundColor": "#800080", matched: false},
-  {"src": "/img/whale.png", "backgroundColor": "#008080", matched: false},
-  {"src": "/img/cat.png", "backgroundColor": "#00ff00", matched: false}
+  { src: "/img/camel.png", backgroundColor: "#ff0000", matched: false },
+  { src: "/img/dog.png", backgroundColor: "#0000ff", matched: false },
+  { src: "/img/fox.png", backgroundColor: "#ffff00", matched: false },
+  { src: "/img/mouse.png", backgroundColor: "#ff00ff", matched: false },
+  { src: "/img/penguin.png", backgroundColor: "#00ffff", matched: false },
+  { src: "/img/rabbit.png", backgroundColor: "#800080", matched: false },
+  { src: "/img/whale.png", backgroundColor: "#008080", matched: false },
+  { src: "/img/cat.png", backgroundColor: "#00ff00", matched: false },
 ];
 
-
 function App() {
+  const [cards, setCards] = useState([]);
 
-  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0);
 
-  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null);
 
-  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
-  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false);
 
-  //mischen 
+  //mischen
   const mischen = () => {
     const gemischt = [...animal_imgs, ...animal_imgs]
       .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random()}))
+      .map((card) => ({ ...card, id: Math.random() }));
 
-      setCards(gemischt)
-      setTurns(0)
-  }
+
+    setChoiceOne(null)
+    setCards(gemischt);
+    setTurns(0);
+  };
+
 
   //Handle Choice
   const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-  }
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
 
   //vergleichen
   useEffect(() => {
     if (choiceOne && choiceTwo) {
+      setDisabled(true);
       if (choiceOne.src === choiceTwo.src) {
-        resetTurn()
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) return { ...card, matched: true };
+            else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1500);
       }
-      else {
-        resetTurn()
-      }
-    }}, [choiceOne, choiceTwo])
-
+    }
+  }, [choiceOne, choiceTwo]);
 
   //neuer turn
   const resetTurn = () => {
-    setChoiceOne = null
-    setChoiceTwo = null 
-    setTurns(prevTurns => prevTurns++)
-  }
+    setChoiceOne = null;
+    setChoiceTwo = null;
+    setTurns((prevTurns) => prevTurns++);
+    setDisabled(false);
+  };
+
+  //Ladestart
+  useEffect(() => {
+    mischen();
+  },[])
 
   return (
     <div className="App">
       <h1>MEGA CUTIE ANIMAL MEMORY</h1>
       <button onClick={mischen}>New Game</button>
       <div className="card-grid">
-        {cards.map(card => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice}/>
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={true}
+          />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,18 +1,23 @@
+import "./SingleCard.css";
 
-export default function SingleCard( {card, handleChoice} ) {
-
-    const handleClick = () => {
-      handleChoice(card)
-      
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
     }
+  };
 
-    return (
-        <div className="card">
-            <div>
-              <img className='front' src={card.src} alt=''/>
-              <img className='back' src='/img/cover.png' onClick={handleClick} alt=''/>
-            </div>
-          </div>
-    )
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          onClick={handleClick}
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
-
